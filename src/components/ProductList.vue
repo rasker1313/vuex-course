564b59f93cc5f69d7ca7b622f92149df1fcdc0e3
artur.y <artur.y@filancy.com>
2022-06-24 16:37:23 +0300

    show product list from file and connect vuex

---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import shop from '@/api/shop'
+export default {
+  name: "ProductList",
+  data(){
+    return{
+      products: []
+    }
+  },
+  created(){
+    shop.getProducts(products => {
+      this.products = products
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+new Vue.Store({
+
+})
