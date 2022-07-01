90f5208283bebb608c9de0255bb0ba16216eafb9
artur.y <artur.y@filancy.com>
2022-07-01 12:22:18 +0300

    Dynamic vuex getters

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -5,7 +5,10 @@
     <ul v-else>
       <li v-for="product in products" :key="product.id">
         {{product.title}} - {{$currency(product.price)}} - {{product.inventory}}
-        <button @click="addProductToCart(product)">Add to cart</button>
+        <button
+            :disabled="!productIsInStock(product)"
+            @click="addProductToCart(product)"
+        >Add to cart</button>
       </li>
     </ul>
   </div>
@@ -21,8 +24,12 @@ export default {
   },
   computed: {
     products (){
-      return this.$store.getters.availableProducts
+      return this.$store.state.products
+    },
+    productIsInStock() {
+      return this.$store.getters.productIsInStock
     }
+
   },
   methods:{
     addProductToCart(product){
--- src/store/index.js
+++ src/store/index.js
@@ -31,6 +31,11 @@ export const store = createStore({
             })
             return total*/
             return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
+        },
+        productIsInStock () {
+            return(product) => {
+                return product.inventory > 0
+            }
         }
     },
 
@@ -48,15 +53,15 @@ export const store = createStore({
 
         },
 
-        addProductToCart(context, product){
-            if(product.inventory > 0){
-                const cartItem = context.state.cart.find(item => item.id === product.id)
+        addProductToCart({state, getters, commit}, product){
+            if(getters.productIsInStock(product)){
+                const cartItem = state.cart.find(item => item.id === product.id)
                 if(!cartItem){
-                    context.commit('pushProductToCart', product.id)
+                    commit('pushProductToCart', product.id)
                 } else {
-                    context.commit('incrementItemQuantity', cartItem)
+                    commit('incrementItemQuantity', cartItem)
                 }
-                context.commit('decrementProductInventory', product)
+                commit('decrementProductInventory', product)
 
             }
         },
